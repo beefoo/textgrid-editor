161aea10b387d4ac37de3431ef182f089cfcc078
beefoo <brian@youaremyjoy.org>
2022-11-08 15:31:59 -0500

    first pass at render word/phones UI

--- public/js/lib/TextGridManager.js
+++ public/js/lib/TextGridManager.js
@@ -9,6 +9,8 @@ class TextGridManager {
     this.isLoading = false;
     this.isLoaded = false;
     this.$filename = $('.textgrid-filename');
+    this.$words = $('#words');
+    this.$transcript = $('#transcript');
   }
 
   loadTextGridFromFile(file) {
@@ -24,10 +26,104 @@ class TextGridManager {
     reader.readAsText(file);
   }
 
+  loadUI() {
+    const { data } = this;
+    // load transcript
+    let html = '';
+    data.forEach((word, i) => {
+      const text = word.text !== '' ? word.text : '_';
+      html += `<button class="word" data-index="${i}">${text}</button>`;
+    });
+    this.$transcript.html(html);
+
+    // load words and phones
+    html = '';
+    data.forEach((word, i) => {
+      const { start, dur } = word;
+      html += `<div class="word" data-word="${i}">`;
+      html += ` <button class="word-text" data-word="${i}">${word.text}</button>`;
+      html += ' <div class="phones">';
+      word.phones.forEach((phone, j) => {
+        const width = ((phone.dur) / dur) * 100;
+        const left = ((phone.start - start) / dur) * 100;
+        const style = `style="width: ${width}%; left: ${left}%"`;
+        html += ` <button class="phone-text" data-word="${i}" data-phone="${j}" ${style}>${phone.text}</button>`;
+      });
+      html += ' </div>';
+      html += '</div>';
+    });
+    this.$words.html(html);
+  }
+
+  loadWord(index) {
+    const wordCount = this.data.length;
+    const wordMargin = 1;
+    let minIndex = index - wordMargin;
+    let maxIndex = index + wordMargin;
+    if (minIndex < 0) {
+      maxIndex -= minIndex;
+      minIndex = 0;
+    }
+    if (maxIndex >= wordCount) {
+      minIndex -= (maxIndex - wordCount + 1);
+      maxIndex = wordCount - 1;
+    }
+    const visibleWords = this.data.filter((word, i) => i >= minIndex && i <= maxIndex);
+    $('.word').removeClass('visible active');
+    const totalDur = visibleWords.reduce((prev, word) => prev + word.dur, 0);
+    const phraseStart = visibleWords[0].start;
+    visibleWords.forEach((word, i) => {
+      const $word = $(`.word[data-word="${word.index}"]`);
+      const width = ((word.dur) / totalDur) * 100;
+      const left = ((word.start - phraseStart) / totalDur) * 100;
+      $word.css({
+        width: `${width}%`,
+        left: `${left}%`,
+      });
+      $word.addClass('visible');
+      if (i === 1) $word.addClass('active');
+    });
+  }
+
   onTextGridLoad(file, data) {
     this.isLoading = false;
+    this.rawData = data;
+    this.data = this.constructor.parseData(data);
+    if (this.data === false) return;
     this.isLoaded = true;
+    // console.log(this.data);
     this.$filename.text(file.name);
-    console.log(data);
+    this.loadUI();
+    this.loadWord(0);
+  }
+
+  static parseData(rawData) {
+    const rawWords = _.findWhere(rawData.items, { name: 'words' });
+    const rawPhones = _.findWhere(rawData.items, { name: 'phones' });
+    if (rawWords === undefined) {
+      alert('TextGrid must have \'words\' entry');
+      return false;
+    }
+    if (rawPhones === undefined) {
+      alert('TextGrid must have \'phones\' entry');
+      return false;
+    }
+    const phones = rawPhones.intervals.map((interval, i) => {
+      const phone = _.clone(interval);
+      phone.start = parseFloat(interval.xmin);
+      phone.end = parseFloat(interval.xmax);
+      phone.dur = phone.end - phone.start;
+      return phone;
+    });
+    const words = rawWords.intervals.map((interval, i) => {
+      const word = _.clone(interval);
+      word.index = i;
+      word.start = parseFloat(interval.xmin);
+      word.end = parseFloat(interval.xmax);
+      word.dur = word.end - word.start;
+      word.phones = _.filter(phones, (phone) => phone.start >= word.start && phone.end <= word.end);
+      return word;
+    });
+    return words;
   }
 }
